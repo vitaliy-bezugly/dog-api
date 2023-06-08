033fdf97b0acaecbbc5f1bc4fee898a668731845
Vitaliy <bezuglivita123@gmail.com>
2023-06-08 13:30:03 +0300

    created migration

---
+++ src/Infrastructure/Persistence/Migrations/20230608102919_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    color = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    tail_length = table.Column<int>(type: "int", nullable: false),
+                    weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
