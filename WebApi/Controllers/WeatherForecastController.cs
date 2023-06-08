cef52a503a0268e661a8e2925dde8113eee185bc
Vitaliy <bezuglivita123@gmail.com>
2023-06-08 10:59:04 +0300

    folders organization

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace RestApi.Controllers
+namespace WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- WebApi/WeatherForecast.cs
+++ WebApi/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace RestApi
+namespace WebApi
 {
     public class WeatherForecast
     {
